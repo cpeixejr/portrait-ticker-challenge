d85ca3c4d211fa3506f1da660636f2128819b4e3
Carlos Peixoto Mangueira Junior <cpeixejr@gmail.com>
2024-06-09 12:29:11 -0300

    test: Fixing ruby initial tests

--- test/controllers/api/v1/tickers_controller_test.rb
+++ test/controllers/api/v1/tickers_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class Api::V1::TickersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_v1_tickers_index_url
+  test "should get success response" do
+    get api_v1_ticker_url("aapl")
     assert_response :success
   end
 end
